5376ab2ed043ae8a40a665a5f6ed11f002e45e64
shjung <seunghyun.jung.84@gmail.com>
2022-11-10 09:48:33 +0900

    Install - Pug

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
 });
+
 module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/vue-page/" : "/",
   outputDir: "docs",
