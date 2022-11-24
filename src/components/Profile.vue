b0cd334e32f7860eb27c7263f6bf7b2d537bf333
shjung <seunghyun.jung.84@gmail.com>
2022-11-24 17:58:38 +0900

    Layout - main

---
+++ src/components/Profile.vue
@@ -0,0 +1,79 @@
+<style lang="scss" scoped>
+.profile {
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  position: relative;
+  z-index: 1;
+  height: 100%;
+  padding: 20px;
+  .inner {
+    overflow: hidden;
+    position: relative;
+    height: 100%;
+    .sec {
+      padding: 10px;
+      -webkit-border-radius: 15px;
+      -moz-border-radius: 15px;
+      border-radius: 15px;
+    }
+  }
+
+  h1 {
+    margin: 15px 0 10px;
+    font-size: 16px;
+  }
+
+  .top-area {
+    height: 50px;
+  }
+  .temperature {
+    height: 100px;
+    margin-top: 15px;
+    background-color: #489992;
+    -webkit-border-radius: 15px;
+    -moz-border-radius: 15px;
+    border-radius: 15px;
+  }
+  .device-area {
+    display: flex;
+    flex-wrap: wrap;
+    overflow: hidden;
+    margin: -5px;
+    .item {
+      width: calc(50% - 10px);
+      min-height: 70px;
+      padding: 10px;
+      margin: 5px;
+      background: #fff;
+      -webkit-border-radius: 15px;
+      -moz-border-radius: 15px;
+      border-radius: 15px;
+    }
+  }
+}
+</style>
+
+<template lang="pug">
+.profile
+  .inner
+    .top-area top-area
+    .temperature temperature
+    h1 {{ msg }}
+    .device-area
+      .item item
+      .item item
+      .item item
+      .item item
+      .item item
+
+</template>
+
+<script>
+export default {
+  name: "MyDevice",
+  props: {
+    msg: String,
+  },
+};
+</script>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,48 +1,55 @@
 <style lang="scss" scoped>
 .dashboard {
+  position: relative;
+  z-index: 2;
   width: 350px;
+  padding: 20px;
+  background-color: #f8f8f6;
+  -webkit-border-radius: 25px;
+  -moz-border-radius: 25px;
+  border-radius: 25px;
 }
 </style>
 
 <template lang="pug">
 .dashboard
-  el-row(:gutter="20" class="row-bg")
-    el-col(:span="6" class="grid-content ep-bg-purple") 1
-    el-col(:span="6" class="grid-content ep-bg-purple") 1
-    el-col(:span="6" class="grid-content ep-bg-purple") 1
-    el-col(:span="6" class="grid-content ep-bg-purple") 1
-  el-row(:gutter="20")
-    el-col(:span="10" class="grid-content ep-bg-purple") 1
-    el-col(:span="10" class="grid-content ep-bg-purple") 1
-  el-row(:gutter="20")
-    el-col(:span="20" class="grid-content ep-bg-purple") 2
-  el-button Button
-  el-button(type="primary") Button
-  el-button(el-button type="danger" circle) A
-  el-button.mdicon(type="danger" plain) #[mdicon(name="playlist-edit" size="19")] Danger
+  .el-wrap
+    el-row(:gutter="20")
+      el-col(:span="12")
+        .grid-content 1
+      el-col(:span="12")
+        .grid-content 1
+    el-row(:gutter="20")
+      el-col(:span="12")
+        .grid-content 1
+      el-col(:span="12")
+        .grid-content 1
+    el-row(:gutter="20")
+      el-col(:span="24")
+        .grid-content 1
 
 
-  el-table(:data="tableData" stripe style="width:100%")
+  //- el-table(:data="tableData" stripe style="width:100%")
     el-table-column(prop="date" label="Date" width="80")
     el-table-column(prop="name" label="Name" width="80")
     el-table-column(prop="address" label="Address")
+
   //- .item item
   //- .item item
   //- .item item
   //- .item item
   //- .item item
   //- .item item
-HelloWorld(msg="info")
+Profile(msg="My Device")
 </template>
 
 <script>
-import HelloWorld from "@/components/HelloWorld.vue";
-// import { Check, Delete, Edit, Message, Search, Star } from "@element-plus/icons-vue";
+import Profile from "@/components/Profile.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    Profile,
   },
   data() {
     return {
